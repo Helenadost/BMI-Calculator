3853fe31c306f64d2bc7b05381936dcc5db3a846
Helenadost <helenadost@seznam.cz>
2023-04-03 22:22:54 +0200

    adding comments

--- script.js
+++ script.js
@@ -16,27 +16,34 @@ document.addEventListener('DOMContentLoaded', () => {
     const classification = document.querySelector(".classification");
     const restart = document.getElementById('restart');
 
+    // iterate over each gender button
     genderBtn.forEach(function (btn, index) {
         btn.onclick = function () {
+            // change img src on click
             genderImg.src = `img/${index}.png`;
 
+            // remove class "active" from all buttons
             removeClass(genderBtn, "active");
+            // Set fill to black for all SVG elements
             fillSvg(genderBtn, "#000");
+            // Add class "active" to the button that was just clicked
             btn.classList.add("active");
+            // Find the SVG within the parent element of the currently clicked button and set the fill to a different color
             const svg = btn.querySelector("svg");
-
             if (svg) {
                 svg.style.fill = "#fff";
             }
         };
     });
 
+    // Function for removing a class from multiple elements
     function removeClass(elements, className) {
         elements.forEach(function (element) {
             element.classList.remove(className);
         });
     };
 
+    // Functions for setting fill for all SVG elements within a given element list
     function fillSvg(elements, fill) {
         elements.forEach(function (element) {
             const svg = element.querySelector("svg");
@@ -47,15 +54,17 @@ document.addEventListener('DOMContentLoaded', () => {
     };
 
     calculation.onclick = function () {
-
         const weightInput = weight.value;
         const heightInput = height.value;
         let input_ok = true;
 
+        // The if conditions verify that the input values are within the specified range and set input_ok to true
         if (weightInput < 20 || weightInput > 300) input_ok = false;
         if (heightInput < 50 || heightInput > 250) input_ok = false;
 
+        // The if condition checks if all input fields are valid (not empty and contain a number) and input_ok is true
         if (fieldsValidation() && input_ok) {
+            // BMI calculation
             const result = (Number(weightInput) / (Number(heightInput / 100) * (heightInput / 100))).toFixed(2);
             resultElement.innerHTML = result;
 
@@ -82,6 +91,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     };
 
+    // function to display the result
     function showResults() {
         resultBox.style.display = "block";
         classification.style.display = "block";
@@ -89,10 +99,12 @@ document.addEventListener('DOMContentLoaded', () => {
         calculationBox.style.display = "none";
     };
 
+    // function to display invalid data notification
     function showFalseValue() {
         falseValue.style.display = "block";
     };
 
+    // The fieldsValidation() function checks if the input fields are valid and returns true or false
     function fieldsValidation() {
         if (weight.value.trim() > "" && !isNaN(weight.value.trim())) {
             if (height.value.trim() > "" && !isNaN(height.value.trim())) {
